52257dcbcdc1835040d6cc278bfbf3adb903f150
SahilSahu <37014214+SahilSahu@users.noreply.github.com>
2019-01-23 13:49:00 +0530

    Update index.js

--- ReactUpload/my-app/src/firebase/index.js
+++ ReactUpload/my-app/src/firebase/index.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
 // Initialize Firebase
-var config = {
-apiKey: "AIzaSyCkMWBO3__-pyJhLUG-agVd_qYdX0Min50",
-authDomain: "react-6d249.firebaseapp.com",
-databaseURL: "https://react-6d249.firebaseio.com",
-projectId: "react-6d249",
-storageBucket: "react-6d249.appspot.com",
-messagingSenderId: "214512394793"
-};
+// firebase config here
 firebase.initializeApp(config);
 
 const storage = firebase.storage();
